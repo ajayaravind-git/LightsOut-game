2f3e99232c1b1d460ffb64b66426085128b978c7
Ajay Aravind <ajayaravind.me@gmail.com>
2022-12-04 13:57:21 +0530

    flip logic implemented to adjuscent cells

---
+++ src/Board.js
@@ -0,0 +1,120 @@
+import React, { Component } from "react";
+import Cell from "./Cell";
+import './Board.css';
+
+
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - hasWon: boolean, true when board is all off
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
+
+class Board extends Component {
+  static defaultProps = {
+    nrows: 5,
+    ncols: 5,
+    chanceLightStartOn: 0.25
+
+
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    };
+
+  }
+
+  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+
+  createBoard() {
+    let board = [];
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        row.push(Math.random() < this.props.chanceLightStartOn)
+      }
+      board.push(row);
+    }
+
+
+    return board;
+  }
+  // create array-of-arrays of true/false values
+
+  /** handle changing a cell: update board & determine if winner */
+
+  flipCellsAround(coord) {
+    let { ncols, nrows } = this.props;
+    let board = this.state.board;
+    let [y, x] = coord.split("-").map(Number);
+
+
+
+    function flipCell(y, x) {
+      if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+        board[y][x] = !board[y][x];
+      }
+    }
+    flipCell(y, x);     //Clicked cell
+    flipCell(y - 1, x);  //left cell
+    flipCell(y + 1, x);   //Right cell
+    flipCell(y, x - 1);    //above cell
+    flipCell(y, x + 1);   //below cell
+
+
+
+    let hasWon = false;
+
+    this.setState({ board: board, hasWon: hasWon });
+  }
+
+
+
+  render() {
+    let tblBoard = [];
+    for (let y = 0; y < this.props.nrows; y++) {
+      let row = [];
+      for (let x = 0; x < this.props.ncols; x++) {
+        let coord = `${y}-${x}`
+        row.push(<Cell key={coord} isLit={this.state.board[y][x]} flipCellsAroundMe={() => this.flipCellsAround(coord)} />)
+      }
+      tblBoard.push(<tr key={y}>{row}</tr>)
+    }
+
+
+    return <div>
+      <table className="Board">
+        <tbody>
+          {tblBoard}
+        </tbody>
+      </table>
+    </div>
+  }
+}
+
+
+export default Board;
